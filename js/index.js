ff689204cab35f6cce5a8a223cba5e10d8050cf6
filecc <fil.gia@live.it>
2023-03-16 19:42:22 +0100

    completed

--- js/index.js
+++ js/index.js
@@ -1,3 +1,45 @@
+const main = document.querySelector('main');
+const paliGame = document.getElementById('pali');
+const pariDispariGame = document.getElementById('paridispari');
+const btnGame1 = document.querySelector('.game1');
+const btnGame2 = document.querySelector('.game2');
+const formPali = document.getElementById('palindrome-form');
+const formPD = document.getElementById('paridispari-form')
+const input = document.querySelector('input[name="palindrome"]');
+const inputPD = document.querySelector('input[name="userNum"]');
+const selected = document.querySelector('select');
+const resultPalindrome = document.querySelector('.resultPalindrome');
+const resultPariDispari = document.querySelector('.resultPariDispari');
+const btnPalindrome = document.querySelector('.btnPalindrome');
+const btnPariDispari = document.querySelector('.btnPariDispari');
+
+
+
+/* choose the game */
+
+btnGame1.addEventListener('click', () => {
+    paliGame.classList.toggle('d-none');
+    btnGame2.classList.toggle('d-none');
+
+    changeButton(
+        btnGame1, 
+        'Palindromi', 
+        'Back Home', 
+        'btn-outline-primary',
+        'btn-outline-danger' );
+});
+
+btnGame2.addEventListener('click', () => {
+    pariDispariGame.classList.toggle('d-none');
+    btnGame1.classList.toggle('d-none');
+    changeButton(
+        btnGame2, 
+        'Pari o Dispari', 
+        'Back Home', 
+        'btn-outline-primary',
+        'btn-outline-danger' );
+});
+
 /*
 
 Esercizio 1
@@ -7,67 +49,21 @@ Creare una funzione per capire se la parola inserita è palindroma
 
 */
 
-const main = document.querySelector('main');
-const paliGame = document.getElementById('pali');
-const btnGame1 = document.querySelector('.game1');
-const btnGame2 = document.querySelector('.game2');
-const form = document.getElementById('palindrome-form');
-const input = document.querySelector('input[name="palindrome"]');
-const resultPalindrome = document.querySelector('.resultPalindrome');
-const btnPalindrome = document.querySelector('.btnPalindrome');
-
-
-
-/* choose the game */
-
-btnGame1.addEventListener('click', () => {
-    paliGame.classList.toggle('d-none');
-    changeButton(
-        btnGame1, 
-        'Palindromi', 
-        'Back Home', 
-        'btn-outline-primary',
-        'btn-outline-danger' );
-});
-
-form.addEventListener('input', () => {
-    if (form.checkValidity()) {   
+formPali.addEventListener('input', () => {
+    if (formPali.checkValidity()) {   
         btnPalindrome.disabled = false;     
     } else {
         btnPalindrome.disabled = true;    
     }
 });
 
-form.addEventListener('submit', (event) => {
+formPali.addEventListener('submit', (event) => {
     event.preventDefault();
     resultPalindrome.append(checkStr(input.value));
     input.value = '';
 });
 
 
-function checkStr(str1){
-    resultPalindrome.innerHTML = '';
-    let strModify = str1.toLowerCase().replace(/[^a-zA-Z0-9]/g, '');
-    let reversed = '';
-    let result;
-    for (let i = strModify.length - 1; i >= 0; i--){
-        reversed += strModify.charAt(i);
-    }
-    if (reversed === strModify) {
-            return result = createChild('h2', ['text-success'], `La stringa "${input.value}" &egrave; un palindromo.`);
-
-    } else {
-        return result = createChild('h2', ['text-danger'], `La stringa "${input.value}" non &egrave; un palindromo.`);
-        
-    }
-
-}
-
-function changeButton(element, firstText, secondText, firstClass, secondClass){
-    element.innerText = element.innerText === firstText ? secondText : firstText;
-    element.classList.toggle(firstClass);
-    element.classList.toggle(secondClass);
-}
 
 /* 
 
@@ -80,3 +76,85 @@ Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione)
 Dichiariamo chi ha vinto.
 
 */
+
+
+formPD.addEventListener('input', () => {
+    if (formPD.checkValidity() && inputPD.value <=5 && inputPD.value >0) {   
+        btnPariDispari.disabled = false;     
+    } else {
+        btnPariDispari.disabled = true;    
+    }
+});
+
+formPD.addEventListener('submit', (event) => {
+    event.preventDefault();
+    let text='', color = '';
+    if (checkWinner(inputPD.value, selected.selectedIndex)){
+        text = 'Hai vinto!';
+        color = ['text-success'];
+        animatedTheBg('success');
+    } else {
+        text = 'Hai perso.';
+        color = ['text-danger'];
+        animatedTheBg('lose');
+    }
+    resultPariDispari.append(createChild('h3', color, text));
+});
+
+/*  FUNCTIONS  */
+function checkWinner(userNum, userChoice) {
+resultPariDispari.innerHTML = '';
+
+  const random = getRandomInt(1, 5);
+  const sum = parseInt(userNum) + random;
+  resultPariDispari.append(createChild('small', '', `&Egrave; uscito: ${sum}`));
+  if (userChoice === 0) {
+    if (sum % 2 === 0) {
+      return true;
+    }
+  } else {
+     if (sum % 2 != 0) {
+      return true;
+    }
+  }
+  return false;
+
+  
+}
+
+
+function checkStr(str1){
+    resultPalindrome.innerHTML = '';
+    let strModify = str1.toLowerCase().replace(/[^a-zA-Z0-9]/g, '');
+    let reversed = '';
+    let result;
+    for (let i = strModify.length - 1; i >= 0; i--){
+        reversed += strModify.charAt(i);
+    }
+    if (reversed === strModify) {
+        animatedTheBg('success');
+            return result = createChild('h2', ['text-success'], `La stringa "${input.value}" &egrave; un palindromo.`);
+
+    } else {
+        animatedTheBg('lose');
+        return result = createChild('h2', ['text-danger'], `La stringa "${input.value}" non &egrave; un palindromo.`);
+        
+    }
+
+}
+
+function changeButton(element, firstText, secondText, firstClass, secondClass){
+    element.innerText = element.innerText === firstText ? secondText : firstText;
+    element.classList.toggle(firstClass);
+    element.classList.toggle(secondClass);
+}
+
+
+function animatedTheBg(className){
+    const divBg = document.getElementById('animatedBg');
+    divBg.classList.remove('animate');
+    divBg.className = className;
+    divBg.offsetWidth;  
+    divBg.classList.add('animate');
+   
+}
