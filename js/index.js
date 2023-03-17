e716666caed27edea09486cf53488d8f7e1ca9a9
filecc <fil.gia@live.it>
2023-03-17 10:09:48 +0100

    changed confetti direction

--- js/index.js
+++ js/index.js
@@ -173,13 +173,13 @@ function celebrate(){
     particleCount: 100,
     angle: 60,
     spread: 55,
-    origin: { x: 0 }
+    origin: { x: 0, y: 1}
   });
   // and launch a few from the right edge
   confetti({
     particleCount: 100,
     angle: 120,
     spread: 55,
-    origin: { x: 1 }
+    origin: { x: 1, y: 1}
   });
 };
